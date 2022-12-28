ea12193f72dd614daf23234ed9fdce6122efacd3
MrProgrammer00 <88051679+MrProgrammer00@users.noreply.github.com>
2022-12-28 20:58:07 +0530

    yes

--- main.js
+++ main.js
@@ -2,3 +2,13 @@ function menuButton () {
     var button = document.getElementById("menu");
     button.classList.toggle("menu");
 }
+
+document.getElementById("home").addEventListener("click", Home);
+document.getElementById("about").addEventListener("click", About);
+document.getElementById("contact").addEventListener("click", Contact);
+document.getElementById("services").addEventListener("click", Services)
+
+function Home() {
+    document.getElementById("Home").classList.add("hidden")
+}
+
