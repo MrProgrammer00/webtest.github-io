3ef04050fc4bd2e2076d720728f2abd1cf2ca195
MrProgrammer00 <88051679+MrProgrammer00@users.noreply.github.com>
2022-12-30 13:22:06 +0530

    yes

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
-function menuButton () {
+function menuButton() {
     var button = document.getElementById("menu");
     button.classList.toggle("menu");
 }
 
-document.getElementById("home").addEventListener("click", Home);
+/*document.getElementById("home").addEventListener("click", Home);
 document.getElementById("about").addEventListener("click", About);
 document.getElementById("contact").addEventListener("click", Contact);
-document.getElementById("services").addEventListener("click", Services)
+document.getElementById("services").addEventListener("click", Services);*/
+
+
 
 function Home() {
-    document.getElementById("Home").classList.add("hidden")
+    document.getElementById("Home").classList.remove("hidden");
+    document.querySelectorAll("#About, #Contact, #Services").forEach(el => el.classList.add("hidden"));
+
 }
 
+function About() {
+    document.getElementById("About").classList.remove("hidden")
+    document.querySelectorAll("#Home, #Contact, #Services").forEach(el => el.classList.add("hidden"));
+}
+
+function Contact() {
+    document.getElementById("Contact").classList.remove("hidden")
+    document.querySelectorAll("#About, #Home, #Services").forEach(el => el.classList.add("hidden"));
+}
+
+function Services() {
+    document.getElementById("Services").classList.remove("hidden")
+    document.querySelectorAll("#About, #Contact, #Home").forEach(el => el.classList.add("hidden"));
+}
